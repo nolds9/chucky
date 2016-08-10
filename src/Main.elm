ef7ffa174531997b54997d7b17e5fefdbce4c776
Nick Olds <nolds9@yahoo.com>
2016-08-10 12:39:38 -0400

    can make an api call and render random quote

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Html exposing (..)
 import Html.App as App
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import Task exposing (Task)
 
 
 -- import String
@@ -16,7 +18,7 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "", Cmd.none )
+    ( Model "", fetchRandomQuoteCmd )
 
 
 
@@ -25,13 +27,41 @@ init =
 
 type Msg
     = GetQuote
+    | FetchQuoteSucess String
+    | HttpError Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GetQuote ->
-            ( { model | quote = model.quote ++ "A quote!" }, Cmd.none )
+            ( model, fetchRandomQuoteCmd )
+
+        FetchQuoteSucess newQuote ->
+            ( { model | quote = newQuote }, Cmd.none )
+
+        HttpError _ ->
+            ( model, Cmd.none )
+
+
+api : String
+api =
+    "http://localhost:3001/"
+
+
+randomQuoteUrl : String
+randomQuoteUrl =
+    api ++ "api/random-quote"
+
+
+fetchRandomQuote : Platform.Task Http.Error String
+fetchRandomQuote =
+    Http.getString randomQuoteUrl
+
+
+fetchRandomQuoteCmd : Cmd Msg
+fetchRandomQuoteCmd =
+    Task.perform HttpError FetchQuoteSucess fetchRandomQuote
 
 
 
